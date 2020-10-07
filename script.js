c1a6a3d18f80f5d91bb28237a3ba7537222523df
terribledevice <longlost@gmail.com>
2020-10-06 21:02:19 -0400

    dog button fetches doggo

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ let button = document.getElementById("generate");
 
 button.addEventListener("click", function() {
     button.innerHTML = "Generated doggo";
-})
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            // console.log(data.message);
+            let dogImage = document.createElement("img");
+            dogImage.setAttribute('src', data.message);
+            document.body.appendChild(dogImage);
+        });
+
+});
